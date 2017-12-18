7792bf0baf5e0cb67ced6af2ff115a553fc799b8
Michael Dunsche <michi.dunsche@gmail.com>
2017-12-18 15:26:18 +0100

    Umsetzung SessionKey

--- src/app/fachlich/padaca.service.ts
+++ src/app/fachlich/padaca.service.ts
@@ -6,6 +6,8 @@ import { RestService } from '../technisch/rest.service';
 @Injectable()
 export class PadacaService {
 
+  sessionkey: string;
+
   constructor(private restService: RestService) { }
 
   //#region Home 
@@ -15,6 +17,13 @@ export class PadacaService {
   //#endregion
 
   //#region Account
+  /**
+   * Führt den Registrierungsprozess für einen User aus.
+   */
+  public getRegister(dto: RegisterDTO): Observable<Response> {
+    return this.restService.postRequest('/register', dto);
+  }
+
   /**
    * Liefert die Session des Users zurück.
    */
@@ -26,21 +35,14 @@ export class PadacaService {
    * Verwirft die Session.
    */
   public getLogout(): Observable<Response> {
-    return this.restService.getRequest('/logout');
-  }
-
-  /**
-   * Führt den Registrierungsprozess für einen User aus.
-   */
-  public getRegister(dto: RegisterDTO): Observable<Response> {
-    return this.restService.postRequest('/register', dto);
+    return this.restService.getRequest('/logout?sessionkey=' + this.sessionkey);
   }
 
   /**
    * Ändert das Passwort eines Users.
    */
   public getChangePassword(dto: ChangePasswordDTO): Observable<Response> {
-    return this.restService.getRequest('/password?old=' + dto.old + '&new=' + dto.new);
+    return this.restService.getRequest('/password?sessionkey=' + this.sessionkey + '&old=' + dto.old + '&new=' + dto.new);
   }
   //#endregion
 
@@ -49,18 +51,18 @@ export class PadacaService {
    * Liefert alle Chatpartner eines Users zurück.
    */
   public getChatPartner(): Observable<Response> {
-    return this.restService.getRequest('/inbox');
+    return this.restService.getRequest('/inbox?sessionkey=' + this.sessionkey);
   }
 
   /**
    * Liefert das Chat-Objekt zu einem Chatpartner zurück, dass alle Nachrichten enthält.
    */
   public getChat(chatPartner: User): Observable<Response> {
-    return this.restService.getRequest('/inbox?userID=' + chatPartner.userID);
+    return this.restService.getRequest('/inbox?sessionkey=' + this.sessionkey + '&userID=' + chatPartner.userID);
   }
 
   public putSendMessage(message: Message): Observable<Response> {
-    return this.restService.putRequest('/inbox', message);
+    return this.restService.putRequest('/inbox?sessionkey=' + this.sessionkey, message);
   }
   //#endregion
 
@@ -69,35 +71,36 @@ export class PadacaService {
    * Erstellt eine Reise.
    */
   public postReiseErstellen(reise: Reise): Observable<Response> {
-    return this.restService.postRequest('/reise/erstellen', reise);
+    return this.restService.postRequest('/reise/erstellen?sessionkey=' + this.sessionkey, reise);
   }
 
   /**
    * Liefert alle Reisen eines Users als Fahrer zurück.
    */
   public getReisenAlsFahrer(user: User): Observable<Response> {
-    return this.restService.getRequest('/reise/fahrer?userID=' + user.userID);
+    return this.restService.getRequest('/reise/fahrer?sessionkey=' + this.sessionkey + '&userID=' + user.userID);
   }
 
   /**
    * Liefert die entsprechende Reise zurück.
    */
   public getReise(reise: Reise): Observable<Response> {
-    return this.restService.getRequest('/reise?reiseID=' + reise.reiseID);
+    return this.restService.getRequest('/reise?sessionkey=' + this.sessionkey + '&reiseID=' + reise.reiseID);
   }
 
   /**
    * Bestätigt oder verweigert die Anfrage eines Mitfahrers.
    */
   public postMitfahrtBestätigen(reise: Reise, user: User, antwort: boolean): Observable<Response> {
-    return this.restService.postRequest('/reise/antwort?reiseID=' + reise.reiseID + '&userID=' + user.userID + '&antwort=' + antwort, {});
+    return this.restService.postRequest('/reise/antwort?sessionkey=' + this.sessionkey
+      + '&reiseID=' + reise.reiseID + '&userID=' + user.userID + '&antwort=' + antwort, {});
   }
 
   /**
    * Sagt die Reise ab & benachrichtigt die betroffenen Personen.
    */
   public postReiseAbsagen(reise: Reise, message: Message) {
-    return this.restService.postRequest('/reise/absagen', message);
+    return this.restService.postRequest('/reise/absagen?sessionkey=' + this.sessionkey, message);
   }
   //#endregion
 
@@ -106,28 +109,29 @@ export class PadacaService {
    * Liefert alle möglichen Mitfahrgelegenheiten für die geplante Route.
    */
   public getSucheReise(route: Route): Observable<Response> {
-    return this.restService.getRequest('/reise/search?start=' + route.start + '&ziel=' + route.ziel + '&datum=' + route.datum);
+    return this.restService.getRequest('/reise/search?sessionkey=' + this.sessionkey
+      + '&start=' + route.start + '&ziel=' + route.ziel + '&datum=' + route.datum);
   }
 
   /**
    * Liefert alle Reisen eines Users als Mitfahrer zurück.
    */
   public getReisenAlsMitfahrer(user: User): Observable<Response> {
-    return this.restService.getRequest('/reise/mitfahrer?userID=' + user.userID);
+    return this.restService.getRequest('/reise/mitfahrer?sessionkey=' + this.sessionkey + '&userID=' + user.userID);
   }
 
   /**
    * Fragt bei dem Fahrer an, ob man mitfahren darf.
    */
   public putReiseAnfragen(reise: Reise): Observable<Response> {
-    return this.restService.putRequest('/reise/anfragen?reiseID=' + reise.reiseID, {});
+    return this.restService.putRequest('/reise/anfragen?sessionkey=' + this.sessionkey + '&reiseID=' + reise.reiseID, {});
   }
 
   /**
    * Sagt die Reise ab & benachrichtigt die betroffenen Personen.
    */
   public postReiseAbmelden(reise: Reise, message: Message) {
-    return this.restService.postRequest('/reise/abmelden?reiseID=' + reise.reiseID, message);
+    return this.restService.postRequest('/reise/abmelden?sessionkey=' + this.sessionkey + '&reiseID=' + reise.reiseID, message);
   }
   //#endregion
 
@@ -136,21 +140,21 @@ export class PadacaService {
    * Speichert die Reise unter den angepinnten Reisen.
    */
   public getPinned(reise: Reise): Observable<Response> {
-    return this.restService.getRequest('/pinned?reiseID=' + reise.reiseID);
+    return this.restService.getRequest('/pinned?sessionkey=' + this.sessionkey + '&reiseID=' + reise.reiseID);
   }
 
   /**
    * Speichert die Reise unter den angepinnten Reisen.
    */
   public putPinned(reise: Reise): Observable<Response> {
-    return this.restService.putRequest('/pinned?reiseID=' + reise.reiseID, {});
+    return this.restService.putRequest('/pinned?sessionkey=' + this.sessionkey + '&reiseID=' + reise.reiseID, {});
   }
 
   /**
    * Löscht die Reise aus den angepinnten Reisen.
    */
   public deletePinned(reise: Reise): Observable<Response> {
-    return this.restService.deleteRequest('/pinned?reiseID=' + reise.reiseID);
+    return this.restService.deleteRequest('/pinned?sessionkey=' + this.sessionkey + '&reiseID=' + reise.reiseID);
   }
   //#endregion
 
@@ -159,28 +163,28 @@ export class PadacaService {
    * Liefert den entsprechenden User mit allen Infos zurück.
    */
   public getUser(user: User): Observable<Response> {
-    return this.restService.getRequest('/profil?userID=' + user.userID);
+    return this.restService.getRequest('/profil?sessionkey=' + this.sessionkey + '&userID=' + user.userID);
   }
 
   /**
    * Ändert das Profil.
    */
   public postChangeProfile(user: User): Observable<Response> {
-    return this.restService.postRequest('/profil', user);
+    return this.restService.postRequest('/profil?sessionkey=' + this.sessionkey, user);
   }
 
   /**
    * Liefert alle von anderen Usern erhaltene Ratings zurück.
    */
   public getBewertungen(user: User): Observable<Response> {
-    return this.restService.getRequest('/bewertungen?userID=' + user.userID);
+    return this.restService.getRequest('/bewertungen?sessionkey=' + this.sessionkey + '&userID=' + user.userID);
   }
 
   /**
    * Erstellt eine Bewertung von einem Fahrer für einen Mitfahrer.
    */
   public putBewertenAlsFahrer(bewertung: Bewertung): Observable<Response> {
-    return this.restService.putRequest('/bewertungen', {
+    return this.restService.putRequest('/bewertungen?sessionkey=' + this.sessionkey, {
       userID: bewertung.mitfahrer ? bewertung.mitfahrer.userID : null,
       rating: bewertung.rating,
       ratingText: bewertung.ratingText
@@ -191,7 +195,7 @@ export class PadacaService {
    * Erstellt eine Bewertung von einem Mitfahrer für einen Fahrer.
    */
   public putBewertenAlsMitfahrer(bewertung: Bewertung): Observable<Response> {
-    return this.restService.putRequest('/bewertungen', {
+    return this.restService.putRequest('/bewertungen?sessionkey=' + this.sessionkey, {
       userID: bewertung.fahrer ? bewertung.fahrer.userID : null,
       rating: bewertung.rating,
       ratingText: bewertung.ratingText
--- src/app/technisch/rest.service.ts
+++ src/app/technisch/rest.service.ts
@@ -11,59 +11,43 @@ export class RestService {
 
   constructor(private http: Http) { }
 
-  private createAuthorizationHeader(): Headers {
-    let header = new Headers();
-    if (sessionStorage.getItem('token')) {
-      header.append('Authorization', 'Bearer ' + sessionStorage.getItem('token'));
-    }
-    return header;
-  }
-
   public getRequest(ressourceAPI: string) {
-    console.log(this.BASEPATH);
-    return this.http.get(this.BASEPATH + ressourceAPI, { headers: this.createAuthorizationHeader() }).map(data => data.json()).catch(
-      (e) => {
-        if (e.status >= 400) {
-          return Observable.throw(e);
-        }
-      });
+    return this.http.get(this.BASEPATH + ressourceAPI).map(data => data.json()).catch((e) => {
+      if (e.status >= 400) {
+        return Observable.throw(e);
+      }
+    });
   }
 
   public postFormRequest(ressourceAPI: string, body: any) {
-    let headers = this.createAuthorizationHeader();
-    headers.append('Content-Type', 'application/x-www-form-urlencoded');
-    return this.http.post(this.BASEPATH + ressourceAPI, body, { headers: headers })
-      .catch((e) => {
-        if (e.status >= 400) {
-          return Observable.throw(e);
-        }
-      });
+    return this.http.post(this.BASEPATH + ressourceAPI, body).catch((e) => {
+      if (e.status >= 400) {
+        return Observable.throw(e);
+      }
+    });
   }
 
   public postRequest(ressourceAPI: string, body: any) {
-    return this.http.post(this.BASEPATH + ressourceAPI, body, { headers: this.createAuthorizationHeader() })
-      .map(data => data.json()).catch((e) => {
-        if (e.status >= 400) {
-          return Observable.throw(e);
-        }
-      });
+    return this.http.post(this.BASEPATH + ressourceAPI, body).map(data => data.json()).catch((e) => {
+      if (e.status >= 400) {
+        return Observable.throw(e);
+      }
+    });
   }
 
   public putRequest(ressourceAPI: string, body: any) {
-    return this.http.put(this.BASEPATH + ressourceAPI, body, { headers: this.createAuthorizationHeader() })
-      .map(data => data.json()).catch((e) => {
-        if (e.status >= 400) {
-          return Observable.throw(e);
-        }
-      });
+    return this.http.put(this.BASEPATH + ressourceAPI, body).map(data => data.json()).catch((e) => {
+      if (e.status >= 400) {
+        return Observable.throw(e);
+      }
+    });
   }
 
   public deleteRequest(ressourceAPI: string) {
-    return this.http.delete(this.BASEPATH + ressourceAPI, { headers: this.createAuthorizationHeader() })
-      .catch((e) => {
-        if (e.status >= 400) {
-          return Observable.throw(e);
-        }
-      });
+    return this.http.delete(this.BASEPATH + ressourceAPI).catch((e) => {
+      if (e.status >= 400) {
+        return Observable.throw(e);
+      }
+    });
   }
 }
