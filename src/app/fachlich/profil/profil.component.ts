2d39ddbd5256cdb4afac5cf6f3dc14af71803f9e
Shoko19961 <Davring@live.de>
2017-12-27 14:23:30 +0100

    Profilseite Layout erstellt + Mockdaten

--- src/app/fachlich/profil/profil.component.ts
+++ src/app/fachlich/profil/profil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Bewertung, User } from '../interfaces';
+import { PadacaService } from '../padaca.service';
 
 @Component({
   selector: 'app-profil',
@@ -6,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profil.component.css']
 })
 export class ProfilComponent implements OnInit {
-
-  constructor() { }
+  bewertungen: Array<Bewertung>;
+  bewertung: number;
+  user: User;
+  constructor(private padaService: PadacaService) { }
 
   ngOnInit() {
+    this.user = {
+      alter: 15,
+      beschreibung: 'Ich fahre gerne Auto... von hier nach da und manchmal auch wieder zurück. Es macht mir einfach Spaß. Aber den Spaß würde ich gerne mit anderen Leuten teilen, indem ich sie mitnehme. Ich habe mich hier bei Padaca registriert, weil Paderborn einfach die beste Stadt in Deutschland ist',
+      userID: 1,
+      vorname: 'Jeder',
+      nachname: 'Bayer',
+      username: 'Immer',
+      pkw: 'Trabbi'
+    };
+    this.bewertung = 0;
+    this.bewertungen = [];
+    this.bewertungen.push({
+      rating: 4.4,
+      reiseID: 1
+    });
+    for (let bew of this.bewertungen) {
+      this.bewertung += bew.rating;
+    }
+    this.bewertung = this.bewertung / this.bewertungen.length;
+    /*let uid = this.padaService.getSession().userID;
+    this.padaService.getUser(uid).subscribe(userres => {
+      this.padaService.getBewertungen(userres.data).subscribe(bewertungres => {
+        this.bewertungen = bewertungres.data;
+        for (let bew of this.bewertungen) {
+          this.bewertung += bew.rating;
+        }
+        this.bewertung = this.bewertung / this.bewertungen.length;
+      });
+    }, error => {
+      this.bewertungen = [];
+      this.bewertungen.push({
+        rating: 10,
+        reiseID: 1
+      });
+    });*/
   }
 
+  private isOwnProfile(): boolean {
+    return false;
+  }
+
+  private editProfile() {
+    console.log('Edit Profile');
+  }
+  private sendMessage() {
+    console.log('Send Message');
+  }
 }
