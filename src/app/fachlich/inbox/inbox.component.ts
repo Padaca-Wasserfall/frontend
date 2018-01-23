221973af22f4cf8834545c686d724bfdc1ec9eb7
Michael Dunsche <michi.dunsche@gmail.com>
2018-01-23 08:06:52 +0100

    Profil ändern

--- src/app/fachlich/inbox/inbox.component.ts
+++ src/app/fachlich/inbox/inbox.component.ts
@@ -1,7 +1,9 @@
+import { ChatComponent } from './chat/chat.component';
 import { Response, Message } from './../interfaces';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Chat, User } from '../interfaces';
 import { PadacaService } from '../padaca.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-inbox',
@@ -10,29 +12,53 @@ import { PadacaService } from '../padaca.service';
 })
 export class InboxComponent implements OnInit {
 
+  @ViewChild('app-chat') chatView: ChatComponent;
   userList: User[];
   selectedChat: Chat;
 
-  constructor(private padacaService: PadacaService) { }
+  constructor(private padacaService: PadacaService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.padacaService.getChatPartner().subscribe((res: Response) => {
       console.log('chatPartner', res);
-      this.userList = res.data;
-      if (this.userList.length > 0) {
-        this.showChat(this.userList[0]);
+      if (res.success) {
+        this.userList = res.data;
+        if (this.userList.length > 0) {
+          this.showChat(this.userList[0].userID);
+          this.chatView.selectChat(this.userList[0]);
+        } else {
+          this.userList = [];
+        }
       }
     }, (err) => {
       console.log('chatPartner', err);
+      this.userList = [];
+    });
+
+    this.route.params.forEach((params: Params) => {
+      let userID = params['userID'];
+      if (userID) {
+        this.showChat(userID);
+      }
     });
   }
 
-  public showChat(selectedUser: User) {
-    this.padacaService.getChat(selectedUser).subscribe((res: Response) => {
-      console.log('getChat', res);
-      this.selectedChat = res.data;
-    }, (err) => {
-      console.log('getChat', err);
+  public showChat(userID: number) {
+    this.padacaService.getChat(userID).subscribe((res1: Response) => {
+      console.log('getChat', res1);
+      this.selectedChat = res1.data;
+    }, (err1) => {
+      console.log('getChat', err1);
+      this.padacaService.getUser(userID).subscribe((res2: Response) => {
+        console.log('user', res2);
+        let chatPartner: User = res2.data;
+        if (chatPartner) {
+          this.userList.push(chatPartner);
+          this.chatView.selectChat(chatPartner); // todo leerer chat bei getChat() als response, wenn chat nicht existiert
+        }
+      }, (err2) => {
+        console.log('user', err2);
+      });
     });
   }
 
@@ -43,6 +69,6 @@ export class InboxComponent implements OnInit {
     }, (err) => {
       console.log('sendMessage', err);
     });
-    this.selectedChat.messages.push(message);
+    this.showChat(this.selectedChat.chatPartner.userID);
   }
 }
